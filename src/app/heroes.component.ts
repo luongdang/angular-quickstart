57733df16ca11ba67766841fe34314fc7d5267de
Luong Dang <luong.dang@gmail.com>
2017-08-27 19:36:04 -0400

    Initial commit
    Initial commit

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router }             from '@angular/router';
+
+
+import { Hero }        from './hero';
+import { HeroService } from './hero.service';
+
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: [ './heroes.component.css' ],
+  providers: [ HeroService ]
+})
+
+export class HeroesComponent implements OnInit {
+    title = 'Tour of Heroes';
+    selectedHero: Hero;
+    heroes: Hero[];
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
+
+    constructor(
+        private heroService: HeroService,
+        private router: Router
+    ) { }
+    getHeroes(): void {
+      this.heroService.getHeroes().then((heroes: Hero[]) => this.heroes = heroes);
+    }
+    ngOnInit(): void {
+        this.getHeroes();
+    }
+
+    gotoDetail(): void {
+      this.router.navigate(['/detail', this.selectedHero.id]);
+    }
+}
